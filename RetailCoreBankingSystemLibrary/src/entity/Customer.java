c3ae519e0142190e62cfe9bef21aef26dcff9459
Michelle Adeline <michelleadelinee@gmail.com>
2021-10-15 16:35:42 +0800

    First Commit

---
+++ RetailCoreBankingSystemLibrary/src/entity/Customer.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author mich
+ */
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    
+    private String firstName;
+    private String lastName;
+    private String identificationNumber;
+    private String contactNumber; 
+    private String adressLine1;
+    private String addressLines2;
+    private String postalCode;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (customerId != null ? customerId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the customerId fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.customerId == null && other.customerId != null) || (this.customerId != null && !this.customerId.equals(other.customerId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Customer[ id=" + customerId + " ]";
+    }
+    
+     /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the identificationNumber
+     */
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    /**
+     * @param identificationNumber the identificationNumber to set
+     */
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    /**
+     * @return the contactNumber
+     */
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    /**
+     * @param contactNumber the contactNumber to set
+     */
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    /**
+     * @return the adressLine1
+     */
+    public String getAdressLine1() {
+        return adressLine1;
+    }
+
+    /**
+     * @param adressLine1 the adressLine1 to set
+     */
+    public void setAdressLine1(String adressLine1) {
+        this.adressLine1 = adressLine1;
+    }
+
+    /**
+     * @return the addressLines2
+     */
+    public String getAddressLines2() {
+        return addressLines2;
+    }
+
+    /**
+     * @param addressLines2 the addressLines2 to set
+     */
+    public void setAddressLines2(String addressLines2) {
+        this.addressLines2 = addressLines2;
+    }
+
+    /**
+     * @return the postalCode
+     */
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    /**
+     * @param postalCode the postalCode to set
+     */
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+    
+}
---
+++ RetailCoreBankingSystemLibrary/src/entity/DepositAccountTransaction.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import util.enumeration.TransactionStatus;
+import util.enumeration.TransactionType;
+
+/**
+ *
+ * @author mich
+ */
+@Entity
+public class DepositAccountTransaction implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long depositAccountTransactionId;
+    
+    private Date transactionDateTime;
+    private TransactionType type;
+    private String code; 
+    private String reference; 
+    private BigDecimal amount;
+    private TransactionStatus status;
+
+    public Long getDepositAccountTransactionId() {
+        return depositAccountTransactionId;
+    }
+
+    public void setDepositAccountTransactionId(Long depositAccountTransactionId) {
+        this.depositAccountTransactionId = depositAccountTransactionId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (depositAccountTransactionId != null ? depositAccountTransactionId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the depositAccountTransactionId fields are not set
+        if (!(object instanceof DepositAccountTransaction)) {
+            return false;
+        }
+        DepositAccountTransaction other = (DepositAccountTransaction) object;
+        if ((this.depositAccountTransactionId == null && other.depositAccountTransactionId != null) || (this.depositAccountTransactionId != null && !this.depositAccountTransactionId.equals(other.depositAccountTransactionId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.DepositAccountTransaction[ id=" + depositAccountTransactionId + " ]";
+    }
+
+    /**
+     * @return the type
+     */
+    public TransactionType getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the status
+     */
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
+    }
+    
+}
